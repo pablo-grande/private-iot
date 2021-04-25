711d1219ff2e9a1a74f3e6aeada2e60e8a65b03e
Pablo Riutort <pablo.riutort@gmail.com>
2021-04-25 17:56:26 +0200

    remove matrix client

--- mqtt/config.py
+++ mqtt/config.py
@@ -4,7 +4,5 @@
 gateway_config = {
     'server': '127.0.0.1',
     'port': 1883,
-    'm_user': 'patient',
-    'm_pass': 'MDTweSomWY'
 }
 
--- mqtt/subscribber.py
+++ mqtt/subscribber.py
@@ -2,15 +2,11 @@
 # -*- coding: utf-8 -*
 import paho.mqtt.client as mqtt
 from config import gateway_config
-from matrix_client.client import MatrixClient
 from requests import put
 
 
 SERVER, PORT = gateway_config['server'], gateway_config['port']
-matrix_username, matrix_password = gateway_config['m_user'], gateway_config['m_pass']
 topic = '#'
-
-messages = []
 
 
 def on_connect(client, userdata, flags, rc):
@@ -25,7 +21,6 @@ def on_message(client, userdata, message):
     payload = message.payload.decode('utf-8').split(":")[-1].strip()
     print(f"{SERVER}/{message.topic}/p={payload} (QoS={message.qos})")
     topic = message.topic.split("/")[-1]
-    room.send_text(f"This is what's sent to your doctor: {payload}")
     # send message to hub
     # TODO: incorporate message broker into hub itself
     put("http://localhost:8000", json={"device": "patient", "topic": topic, "payload": payload})
@@ -33,21 +28,8 @@ def on_message(client, userdata, message):
 
 
 matrix_client = MatrixClient("http://localhost:8008")
-token = matrix_client.login(username=matrix_username, password=matrix_password)
-room = matrix_client.join_room("!qKGqWURPTdcyFQHFjJ:casper.magi.sys")
 client = mqtt.Client('subscribber', transport='tcp')
 client.on_connect = on_connect
 client.on_message = on_message
 client.connect(SERVER, PORT)
 client.loop_forever()
-
-
-
-
-# New user
-# token = client.register_with_password(username="someother_name", password="kark6424")
-
-# Existing user
-set_trace()
-
-
